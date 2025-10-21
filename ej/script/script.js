9222108a9c150efb777b99eb7dc93d7b22252200
S-3311 <100522360@alumnos.uc3m.es>
2025-10-21 18:57:04 +0200

    Añadidos

---
+++ ej/script/script.js
@@ -0,0 +1,12 @@
+const abc= (a) =>
+{
+    alert ("no me pulses");
+    console.log("sdsddw");
+    console.dir(a);
+
+    alert(document.querySelector("#campo").value);
+
+};
+
+document.querySelector("#pulsable").addEventListener("click", abc);
+alert("Hola")
