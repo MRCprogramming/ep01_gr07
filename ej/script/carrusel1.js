fa27d7be8e7e3b89cccb41c540640b98f9db92eb
Marcos Rodríguez Cuadrado <100522136@alumnos.uc3m.es>
2025-10-25 16:15:57 +0200

    carrusel autoslide

--- ej/script/carrusel1.js
+++ ej/script/carrusel1.js
@@ -1,15 +1,18 @@
 const leftBtn = document.querySelector('.btn-left');
 const rightBtn = document.querySelector('.btn-right');
 let contador = 0;
+let autoSlideTimer;
 
 leftBtn.addEventListener('click', () => {
   contador = (contador + 2)%3;
   updateContainer(contador);
+  iniciarAutoSlide();
 });
 
 rightBtn.addEventListener('click', () => {
     contador = (contador + 1)%3;
     updateContainer(contador);
+    iniciarAutoSlide();
 });
 
 function updateContainer(num) {
@@ -25,4 +28,15 @@ function updateContainer(num) {
     document.querySelector('.slide1').style.display = 'none';
     document.querySelector('.slide2').style.display = 'none';
     document.querySelector('.slide3').style.display = 'block';
-  }}
+  }
+}
+
+function iniciarAutoSlide() {
+  // Cambio cada 3 segs
+  autoSlideTimer = setInterval(() => {
+    contador = (contador + 1) % 3;
+    updateContainer(contador);
+  }, 3000);
+}
+
+iniciarAutoSlide(); // Iniciar autoslide
