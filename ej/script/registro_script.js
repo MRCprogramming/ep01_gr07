c101f7448d404e8ca2834a838bec7a50324a7445
S-3311 <100522360@alumnos.uc3m.es>
2025-10-25 12:28:29 +0200

    Commit de ayer

---
+++ ej/script/registro_script.js
@@ -0,0 +1,4 @@
+function Interaccion() {
+document.getElementById("nombre").maxLength = "3";
+document.getElementById("demo").innerHTML = "Maximum number of characters allowed in the text area is now 3";
+};
